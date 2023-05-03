3a05bbd5ded8513f056886fe4e97ded19678243f
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-03 19:20:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const div = document.getElementById("sizeInfo");
+window.addEventListener('resize', updateSize);
+function updateSize() {
+	div.innerText="";
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+	const h1 = document.createElement("h1");
+	h1.innerText=`Window: ${width} and Height: ${height}`;
+	div.appendChild(h1);
+}
